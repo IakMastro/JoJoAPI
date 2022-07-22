308b729a008d71ba1355ab9295df34e057e52cd3
Mario <papazogloy59@gmail.com>
2022-07-22 16:24:32 +0300

    implemented caching and fixed out of bounds bug in get_stand

--- api/app.py
+++ api/app.py
@@ -4,25 +4,41 @@ import numpy as np
 from fastapi import FastAPI
 from csv import DictReader
 
+from functools import lru_cache
+
 with open("./jojo-stand-stats.csv", newline='', encoding='utf-8') as file:
     
     data : list[dict] = [ stand for stand in DictReader(file) ] 
 
     data : np.ndarray = np.array(data)
 
+
 app = FastAPI()
 
+
+@lru_cache(2000)
 @app.get("/api/v2")
 async def default():
     return {"_message": "Hello, You are connected with JoJo API"}
 
+
+@lru_cache(2000)
 @app.get("/api/v2/stands")
 async def get_all_stands(): 
     return list(data) 
 
+@lru_cache(2000)
 @app.get("/api/v2/stands/{stand_id}")
 async def get_stand(stand_id: int):
+
+    if stand_id >= len(data):
+        return {"_message": "stand out of bounds"}
+
     return data[stand_id]
+
+
+
+
 
 
 """
