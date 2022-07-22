96fb98c978e4c7cd01401c651df656120925195b
Mario <papazogloy59@gmail.com>
2022-07-22 19:25:49 +0300

    cleaned

--- api/app.py
+++ api/app.py
@@ -1,5 +1,4 @@
-import pymongo as mongo 
-import numpy as np 
+import pymongo as mongo  
 
 from fastapi import FastAPI
 from csv import DictReader
@@ -9,37 +8,6 @@ from functools import lru_cache
 with open("./jojo-stand-stats.csv", newline='', encoding='utf-8') as file:
     
     data : list[dict] = [ stand for stand in DictReader(file) ] 
-
-    data : np.ndarray = np.array(data)
-
-
-app = FastAPI()
-
-
-@lru_cache(2000)
-@app.get("/api/v2")
-async def default():
-    return {"_message": "Hello, You are connected with JoJo API"}
-
-
-@lru_cache(2000)
-@app.get("/api/v2/stands")
-async def get_all_stands(): 
-    return list(data) 
-
-@lru_cache(2000)
-@app.get("/api/v2/stands/{stand_id}")
-async def get_stand(stand_id: int):
-
-    if stand_id >= len(data):
-        return {"_message": "stand out of bounds"}
-
-    return data[stand_id]
-
-
-
-
-
 
 """
 TODO:
@@ -55,3 +23,26 @@ TODO:
 
 # How to run program
 # uvicorn app:app --reload  (--reload for debugging)
+
+
+app = FastAPI()
+
+@lru_cache(2000)
+@app.get("/api/v2")
+async def default():
+    return {"_message": "Hello, You are connected with JoJo API"}
+
+
+@lru_cache(2000)
+@app.get("/api/v2/stands")
+async def get_all_stands(): 
+    return data
+
+@lru_cache(2000)
+@app.get("/api/v2/stands/{stand_id}")
+async def get_stand(stand_id: int):
+
+    if stand_id >= len(data):
+        return {"_message": "stand out of bounds"}
+
+    return data[stand_id]
